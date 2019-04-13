c4e8ff9a833723d96674f08ed0fdde4c09d538d2
matometaru <matometaru@gmail.com>
2019-04-13 18:56:42 +0900

    first commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,62 @@
+const main = require('../index');
+
+describe('validator', () => {
+    test('正しい入力値', () => {
+        const params = {
+            string_ids: "1,2,abc,3,1.2",
+            user_ids: [1,2],
+        };
+        const actual = main.validator(params)
+        const expected = {
+            string_ids: "1,2,abc,3,1.2",
+            user_ids: [1,2],
+        };
+        expect(actual).toEqual(expected);
+    });
+    test('user_idsが未入力', () => {
+        const params = {
+            string_ids: "1,2,abc,3,1.2",
+        };
+        try {
+            main.validator(params);
+        } catch (e) {
+            expect(e.message).toEqual(`"user_ids" is required`);
+        }
+    });
+    test('string_idsが未入力', () => {
+        const params = {
+            user_ids: [1,2],
+        };
+        try {
+            main.validator(params);
+        } catch (e) {
+            expect(e.message).toEqual(`"string_ids" is required`);
+        }
+    });
+});
+
+describe('converter', () => {
+    test('正しい入力値', () => {
+        const params = {
+            string_ids: "1,2,3",
+            user_ids: [1,2],
+        };
+        const actual = main.converter(params)
+        const expected = {
+            stringIds: [1,2,3],
+            userIds: [1,2],
+        };
+        expect(actual).toEqual(expected);
+    });
+    test('string_idsにカンマが2連続で含まれている', () => {
+        const params = {
+            string_ids: "1,2,,3",
+            user_ids: [1,2],
+        };
+        try {
+            main.converter(params);
+        } catch (e) {
+            expect(e.message).toEqual(`params must be larger than or equal to 1`);
+        }
+    });
+});
---
+++ index.js
@@ -0,0 +1,60 @@
+const Joi = require('joi');
+
+// 入力値に対する厳密なバリデーション
+function validator(params) {
+    // 検証ルールを定義
+    const schema = Joi.object().keys({
+        string_ids: Joi.string().required(),
+        user_ids: Joi.array().items(Joi.number()).required(),
+    });
+
+    const result = Joi.validate(params, schema, { convert: false });
+    if (result.error) {
+        const errorMessage = result.error.details[0].message;
+        const errorType = result.error.details[0].type;
+        switch (errorType) {
+            case "any.required":
+                throw new Error(errorMessage);
+            default:
+                throw new Error("その他のエラー");
+        }
+    }
+    return result.value;
+}
+
+// キーと型を変換
+function converter(params) {
+    // キーと型を変換
+    const convertedParams = {
+        stringIds: params.string_ids.split(",").map((v) => +v),
+        userIds: params.user_ids,
+    };
+
+    // 検証ルールを定義
+    const schema = Joi.object().keys({
+        stringIds: Joi.array().items(Joi.number().min(1).integer()).required(),
+        userIds: Joi.array().items(Joi.number()).required(),
+    });
+
+    // サービスに渡す前の正当性検証
+    // 別関数に分けるべき？
+    const result = Joi.validate(convertedParams, schema, { convert: false });
+    if (result.error) {
+        const errorMessage = result.error.details[0].message;
+        const errorType = result.error.details[0].type;
+        switch (errorType) {
+            case "any.required":
+                throw new Error(errorMessage);
+            case "number.min":
+                throw new Error(`params must be larger than or equal to 1`);
+            default:
+                throw new Error("その他のエラー");
+        }
+    }
+    return result.value;
+}
+
+module.exports = {
+    validator,
+    converter
+};
